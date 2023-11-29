fef5ea585e2ee271f085b867b1e663f27c334ebc
Denny Lungu <21274541+Lungu19@users.noreply.github.com>
2023-11-29 19:46:36 +0100

    Added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+<div class="container mx-auto flex items-center justify-center flex-col">
+    <p class="text-2xl text-justify font-bold">Error {$page.status}</p>
+    <p>{$page.error.message}</p>
+    <a href=".">Go to the homepage</a>
+</div>
